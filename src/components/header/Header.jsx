4c98797d5b115515f6322e81460d33336f87475b
Ariel-Fortalisa <arielfortalisa0315@gmail.com>
2025-01-27 10:51:44 +0800

    message

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,118 +1,119 @@
-import React from 'react';
-import "./header.css";
-import { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
+import './header.css';
 
 const Header = () => {
+  const [toggle, showMenu] = useState(false);
+  const [activeNav, setActiveNav] = useState('#home');
 
-    /*============ Change Background Header ============*/
-    window.addEventListener("scroll", function () {
-        const header = document.querySelector(".header");
-        // when the scroll is higher than 200 viewport height, add the scroll-header class to a tag with the header tag
-        if (this.scrollY >= 80) header.classList.add("scroll-header"); 
-        else header.classList.remove("scroll-header");
-
-        // Close the menu when scrolling
-        if (Toggle) {
-            showMenu(false);
-        }
-    });
-
-    /*============ Toggle Menu ============*/
-    const[Toggle, showMenu] = useState(false); 
-    const [activeNav, setActiveNav] = useState("#home");
-    
-    /*============ Active Link on Scroll ============*/
-    useEffect(() => {
-        const sections = document.querySelectorAll('section');
-        const options = {
-            rootMargin: '0px 0px -50% 0px', // Trigger when the section is at the center of the viewport
-        };
-
-        const observer = new IntersectionObserver((entries) => {
-            entries.forEach((entry) => {
-                if (entry.isIntersecting) {
-                    setActiveNav(`#${entry.target.id}`);
-                }
-            });
-        }, options);
-
-        sections.forEach((section) => observer.observe(section));
-
-        return () => observer.disconnect();
-    }, []);
-
-    const handleNavClick = (id) => {
-        setActiveNav(id);
-        showMenu(false); // Close the menu on link click
+  /*============ Change Background Header ============*/
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('.header');
+      if (window.scrollY >= 80) {
+        header.classList.add('scroll-header');
+      } else {
+        header.classList.remove('scroll-header');
+      }
     };
 
-    return (
-        <header className="header">
-            <nav className="nav container">
-                <a href="index.html" className="nav_logo">Portfolio</a>
+    window.addEventListener('scroll', handleScroll);
+    return () => window.removeEventListener('scroll', handleScroll);
+  }, []);
 
-                <div className={Toggle ? "nav_menu show-menu" : "nav_menu"}>
-                    <ul className="nav_list grid">
-                        <li className="nav_item">
-                            <a 
-                                href="#home" 
-                                onClick={() => handleNavClick("#home")}
-                                className={activeNav === "#home" ? "nav_link active-link" : "nav_link"}
-                            >
-                                <i className="uil uil-estate nav_icon"></i> Home
-                            </a>
-                        </li>
+  /*============ Active Link on Scroll ============*/
+  useEffect(() => {
+    const sections = document.querySelectorAll('section');
+    const options = {
+      rootMargin: '0px 0px -50% 0px', // Trigger when the section is at the center of the viewport
+    };
 
-                        <li className="nav_item">
-                            <a 
-                                href="#about" 
-                                onClick={() => handleNavClick("#about")}
-                                className={activeNav === "#about" ? "nav_link active-link" : "nav_link"}
-                            >
-                                <i className="uil uil-user nav_icon"></i> About
-                            </a>
-                        </li>
+    const observer = new IntersectionObserver((entries) => {
+      entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+          setActiveNav(`#${entry.target.id}`);
+        }
+      });
+    }, options);
 
-                        <li className="nav_item">
-                            <a 
-                                href="#skills" 
-                                onClick={() => handleNavClick("#skills")}
-                                className={activeNav === "#skills" ? "nav_link active-link" : "nav_link"}
-                            >
-                                <i className="uil uil-file-alt nav_icon"></i> Skills
-                            </a>
-                        </li>
+    sections.forEach((section) => observer.observe(section));
 
-                        <li className="nav_item">
-                            <a 
-                                href="#projects" 
-                                onClick={() => handleNavClick("#projects")}
-                                className={activeNav === "#projects" ? "nav_link active-link" : "nav_link"}
-                            >
-                                <i className="uil uil-scenery nav_icon"></i> Projects
-                            </a>
-                        </li>
+    return () => observer.disconnect();
+  }, []);
 
-                        <li className="nav_item">
-                            <a 
-                                href="#contact" 
-                                onClick={() => handleNavClick("#contact")}
-                                className={activeNav === "#contact" ? "nav_link active-link" : "nav_link"}
-                            >
-                                <i className="uil uil-message nav_icon"></i> Contact
-                            </a>
-                        </li>
-                    </ul>
+  const handleNavClick = (id) => {
+    setActiveNav(id);
+    showMenu(false); // Close the menu on link click
+  };
 
-                    <i className="uil uil-times nav_close" onClick={() => showMenu(!Toggle)}></i>
-                </div>
+  return (
+    <header className="header">
+      <nav className="nav container">
+        <a href="index.html" className="nav_logo">
+          Portfolio
+        </a>
 
-                <div className="nav_toggle" onClick={() => showMenu(!Toggle)}>
-                    <i className="uil uil-apps"></i>
-                </div>
-            </nav>
-        </header>
-    )
-}
+        <div className={toggle ? 'nav_menu show-menu' : 'nav_menu'}>
+          <ul className="nav_list grid">
+            <li className="nav_item">
+              <a
+                href="#home"
+                onClick={() => handleNavClick('#home')}
+                className={activeNav === '#home' ? 'nav_link active-link' : 'nav_link'}
+              >
+                <i className="uil uil-estate nav_icon"></i> Home
+              </a>
+            </li>
+
+            <li className="nav_item">
+              <a
+                href="#about"
+                onClick={() => handleNavClick('#about')}
+                className={activeNav === '#about' ? 'nav_link active-link' : 'nav_link'}
+              >
+                <i className="uil uil-user nav_icon"></i> About
+              </a>
+            </li>
+
+            <li className="nav_item">
+              <a
+                href="#skills"
+                onClick={() => handleNavClick('#skills')}
+                className={activeNav === '#skills' ? 'nav_link active-link' : 'nav_link'}
+              >
+                <i className="uil uil-file-alt nav_icon"></i> Skills
+              </a>
+            </li>
+
+            <li className="nav_item">
+              <a
+                href="#projects"
+                onClick={() => handleNavClick('#projects')}
+                className={activeNav === '#projects' ? 'nav_link active-link' : 'nav_link'}
+              >
+                <i className="uil uil-scenery nav_icon"></i> Projects
+              </a>
+            </li>
+
+            <li className="nav_item">
+              <a
+                href="#contact"
+                onClick={() => handleNavClick('#contact')}
+                className={activeNav === '#contact' ? 'nav_link active-link' : 'nav_link'}
+              >
+                <i className="uil uil-message nav_icon"></i> Contact
+              </a>
+            </li>
+          </ul>
+
+          <i className="uil uil-times nav_close" onClick={() => showMenu(!toggle)}></i>
+        </div>
+
+        <div className="nav_toggle" onClick={() => showMenu(!toggle)}>
+          <i className="uil uil-apps"></i>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
 export default Header;
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -1,9 +1,47 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 import "./projects.css";
 import Img1 from "../../assets/ordering.jpg";
+import Img2 from "../../assets/ordering(2).jpg";
+import Img3 from "../../assets/ordering(3).jpg";
+import Img4 from "../../assets/ordering(4).jpg";
+import Img5 from "../../assets/ordering(5).jpg";
 
 const Projects = () => {
+    const projects = [
+        {
+            title: "Online Ordering Web Application for Romanteaco Milk Tea Shop",
+            description:
+                "Developed as our capstone system project that simplifies the ordering process for milk tea shops by managing transactions, automating inventory, and tracking overall sales.",
+            images: [Img1, Img2, Img3, Img4, Img5],
+        },
+        {
+            title: "Web-Based Sales Monitoring and Inventory System for Jack Sizzling Avenue",
+            description:
+                "A Web-Based Sales Monitoring and Inventory System was developed as a freelance developer that integrated digital sales transactions to ensure smooth and error-free processing.",
+            images: [Img2, Img1, Img3],
+        },
+    ];
+
+    const [currentSlides, setCurrentSlides] = useState(
+        projects.map(() => 0) // Initialize each project's slide index to 0
+    );
     const [toggleState, setToggleState] = useState(0);
+
+    const handleNextSlide = (projectIndex, slides) => {
+        setCurrentSlides((prevSlides) =>
+            prevSlides.map((slide, index) =>
+                index === projectIndex ? (slide + 1) % slides.length : slide
+            )
+        );
+    };
+
+    const handlePrevSlide = (projectIndex, slides) => {
+        setCurrentSlides((prevSlides) =>
+            prevSlides.map((slide, index) =>
+                index === projectIndex ? (slide - 1 + slides.length) % slides.length : slide
+            )
+        );
+    };
 
     const toggleTab = (index) => {
         setToggleState(index);
@@ -15,166 +53,107 @@ const Projects = () => {
             <span className="section_subtitle">What I Develop</span>
 
             <div className="projects_container container grid">
-                {/* Project 1 */}
-                <div className="projects_content">
-                    <div className="projects_details">
-                        <img src={Img1} alt="" className="project_img" />
-                        <h3 className="projects_title">
-                            Online Ordering Web Application for Romanteaco Milk Tea Shop
-                        </h3>
-                    </div>
-                    <div className="project-demo">
-                        <span
-                            className="projects_button"
-                            onClick={() => toggleTab(1)}
-                        >
-                            View More
-                            <i className="uil uil-arrow-right projects_button-icon"></i>
-                        </span>
+                {projects.map((project, index) => (
+                    <div className="projects_content" key={index}>
+                        {/* Slideshow */}
+                        <div className="projects_details">
+                            <div className="slideshow">
+                                <div
+                                    className="slideshow_inner"
+                                    style={{
+                                        transform: `translateX(-${currentSlides[index] * 100}%)`,
+                                        width: `${project.images.length * 100}%`, // Adjust width dynamically
+                                    }}
+                                >
+                                    {project.images.map((img, imgIndex) => (
+                                        <img
+                                            src={img}
+                                            alt={`Slide ${imgIndex}`}
+                                            className="project_img"
+                                            key={imgIndex}
+                                        />
+                                    ))}
+                                </div>
+                                <button
+                                    className="slideshow_button prev"
+                                    onClick={() => handlePrevSlide(index, project.images)}
+                                >
+                                    &#10094;
+                                </button>
+                                <button
+                                    className="slideshow_button next"
+                                    onClick={() => handleNextSlide(index, project.images)}
+                                >
+                                    &#10095;
+                                </button>
+                            </div>
+                            <h3 className="projects_title">{project.title}</h3>
+                        </div>
 
-                        <span
-                            className="projects_button-github"
-                            onClick={() => toggleTab(1)}
+                        {/* Buttons */}
+                        <div className="project-demo">
+                            <span
+                                className="projects_button"
+                                onClick={() => toggleTab(index + 1)}
+                            >
+                                View More
+                                <i className="uil uil-arrow-right projects_button-icon"></i>
+                            </span>
+
+                            <span
+                                className="projects_button-github"
+                                onClick={() => window.open("https://github.com", "_blank")}
+                            >
+                                GitHub
+                                <i className="uil uil-github projects_button-icon"></i>
+                            </span>
+                        </div>
+
+                        {/* Modal */}
+                        <div
+                            className={
+                                toggleState === index + 1
+                                    ? "projects_modal active-modal"
+                                    : "projects_modal"
+                            }
                         >
-                            Github
-                            <i className="uil uil-github projects_button-icon"></i>
-                        </span>
-                    </div>
-                    
-                    <div
-                        className={
-                            toggleState === 1
-                                ? "projects_modal active-modal"
-                                : "projects_modal"
-                        }
-                    >
-                        <div className="projects_modal-content">
-                            <i
-                                onClick={() => toggleTab(0)}
-                                className="uil uil-times projects_modal-close"
-                            ></i>
-                            <h3 className="projects_modal-title">
-                                Online Ordering Web Application for Romanteaco Milk Tea Shop
-                            </h3>
-                            <p className="projects_modal-description">
-                                Developed as our capstone system project that simplifies the
-                                ordering process for milk tea shops by managing transactions,
-                                automating inventory, and tracking overall sales. It enhances
-                                efficiency by reducing manual tasks and ensuring accurate data
-                                management.
-                            </p>
-                            <ul className="projects_modal-projects grid">
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">HTML5</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">CSS</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">JavaScript</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">Bootstrap</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">PHP</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">MySQL</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">Hostinger</p>
-                                </li>
-                            </ul>
+                            <div className="projects_modal-content">
+                                <i
+                                    onClick={() => toggleTab(0)}
+                                    className="uil uil-times projects_modal-close"
+                                ></i>
+                                <h3 className="projects_modal-title">{project.title}</h3>
+                                <p className="projects_modal-description">{project.description}</p>
+                                <ul className="projects_modal-projects grid">
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">HTML5</p>
+                                    </li>
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">CSS</p>
+                                    </li>
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">JavaScript</p>
+                                    </li>
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">Bootstrap</p>
+                                    </li>
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">PHP</p>
+                                    </li>
+                                    <li className="projects_modal-project">
+                                        <i className="uil uil-check-circle projects_modal-icon"></i>
+                                        <p className="projects_modal-info">MySQL</p>
+                                    </li>
+                                </ul>
+                            </div>
                         </div>
                     </div>
-                </div>
-
-                {/* Project 2 */}
-                <div className="projects_content">
-                    <div className="projects_details">
-                        <img src={Img1} alt="" className="project_img" />
-                        <h3 className="projects_title">
-                            Web-Based Sales Monitoring and Inventory System for Jack Sizzling Avenue
-                        </h3>
-                    </div>
-
-                    <div className="project-demo">
-                        <span
-                            className="projects_button"
-                            onClick={() => toggleTab(2)}
-                        >
-                            View More
-                            <i className="uil uil-arrow-right projects_button-icon"></i>
-                        </span>
-
-                        <span
-                            className="projects_button-github"
-                            onClick={() => toggleTab(1)}
-                        >
-                            Github
-                            <i className="uil uil-github projects_button-icon"></i>
-                        </span>
-                    </div>
-                    
-                    <div
-                        className={
-                            toggleState === 2
-                                ? "projects_modal active-modal"
-                                : "projects_modal"
-                        }
-                    >
-                        <div className="projects_modal-content">
-                            <i
-                                onClick={() => toggleTab(0)}
-                                className="uil uil-times projects_modal-close"
-                            ></i>
-                            <h3 className="projects_modal-title">
-                                Web-Based Sales Monitoring and Inventory System for Jack Sizzling Avenue
-                            </h3>
-                            <p className="projects_modal-description">
-                                A Web-Based Sales Monitoring and Inventory System was developed
-                                as a freelance developer that integrated digital sales transactions
-                                to ensure smooth and error-free processing. All transactions were
-                                systematically logged and recorded within the system for accurate
-                                and reliable tracking.
-                            </p>
-                            <ul className="projects_modal-projects grid">
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">HTML5</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">CSS</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">JavaScript</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">Bootstrap</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">PHP</p>
-                                </li>
-                                <li className="projects_modal-project">
-                                    <i className="uil uil-check-circle projects_modal-icon"></i>
-                                    <p className="projects_modal-info">MySQL</p>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+                ))}
             </div>
         </section>
     );
